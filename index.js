c5df76bc5ecfa6d15cf10156541722eb78ff0a82
morou <kazuhiro.nakae@gmail.com>
2014-02-05 23:30:23 +0900

    update test

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ var PluginError = require('gulp-util').PluginError;
 
 module.exports = function (editor) {
 
-  if (!editor) throw new PluginError('gulp-json-editor', 'missing "editor function" option for gulp-json-editor');    
-  if (typeof editor !== 'function') throw new PluginError('gulp-json-editor', '"editor function" option must be Function');    
+  // check options
+  if (!editor)
+    throw new PluginError('gulp-json-editor', 'missing "editor function" option for gulp-json-editor');    
+  if (typeof editor !== 'function')
+    throw new PluginError('gulp-json-editor', '"editor function" option must be Function');    
 
-  return through.obj(function (file) {
+  return through.obj(function (file, encoding, callback) {
 
-    if (file.isNull()) return;
-    if (file.isStream()) return this.emit('error', new PluginError('gulp-json-editor',  'Streaming not supported'));
+    // ignore it
+    if (file.isNull()) {
+      return callback();
+    }
 
+    // stream is not supported
+    if (file.isStream()) {
+      this.emit('error', new PluginError('gulp-json-editor', 'Streaming not supported'));
+      return callback();
+    }
+
+    // edit JSON object
     try {
       file.contents = new Buffer(JSON.stringify(editor(JSON.parse(file.contents.toString('utf8')))));
     }
@@ -18,6 +30,7 @@ module.exports = function (editor) {
       this.emit('error', new PluginError('gulp-json-editor', err));
     }
     this.push(file);
+    callback();
 
   });
 
---
+++ test/test.js
@@ -0,0 +1,126 @@
+var json = require('../');
+var gulp = require("gulp");
+should = require('should');
+require('mocha');
+
+describe('gulp-json-editor', function() {
+  describe('#json()', function() {
+
+    //
+    // test: modify property
+    //
+    it('should modify property of JSON object', function(done) {
+      var stream = gulp.src('test/test.json').pipe(json(function(obj) {
+        obj.version = '2.0.0';
+        return obj;
+      }));
+      stream.on('error', done);
+      stream.on('data', function(file) {
+        var obj = JSON.parse(file.contents);
+        obj.should.have.properties({
+          'name': 'test object',
+          'version': '2.0.0'
+        });
+      });
+      stream.on('end', done);
+    });
+
+    //
+    // test: add property
+    //
+    it('should add property of JSON object', function(done) {
+      var stream = gulp.src('test/test.json').pipe(json(function(obj) {
+        obj.description = 'this is test';
+        return obj;
+      }));
+      stream.on('error', done);
+      stream.on('data', function(file) {
+        var obj = JSON.parse(file.contents);
+        obj.should.have.properties({
+          'name': 'test object',
+          'version': '1.0.0',
+          'description': 'this is test'
+        });
+      });
+      stream.on('end', done);
+    });
+
+    //
+    // test: remove property
+    //
+    it('should remove property of JSON object', function(done) {
+      var stream = gulp.src('test/test.json').pipe(json(function(obj) {
+        delete obj.name;
+        return obj;
+      }));
+      stream.on('error', done);
+      stream.on('data', function(file) {
+        var obj = JSON.parse(file.contents);
+        obj.should.have.properties({
+          'version': '1.0.0'
+        });
+        obj.should.not.have.property('name');
+      });
+      stream.on('end', done);
+    });
+
+    //
+    // test: modify nested property
+    //
+    it('should modify nested property of JSON object', function(done) {
+      var stream = gulp.src('test/test.json').pipe(json(function(obj) {
+        obj.nested.version = '2.0.0';
+        return obj;
+      }));
+      stream.on('error', done);
+      stream.on('data', function(file) {
+        var obj = JSON.parse(file.contents);
+        obj.nested.should.have.properties({
+          'name': 'nested object',
+          'version': '2.0.0'
+        });
+      });
+      stream.on('end', done);
+    });
+
+    //
+    // test: add nested property
+    //
+    it('should add nested property of JSON object', function(done) {
+      var stream = gulp.src('test/test.json').pipe(json(function(obj) {
+        obj.nested.description = 'this is test';
+        return obj;
+      }));
+      stream.on('error', done);
+      stream.on('data', function(file) {
+        var obj = JSON.parse(file.contents);
+        obj.nested.should.have.properties({
+          'name': 'nested object',
+          'version': '1.0.0',
+          'description': 'this is test'
+        });
+      });
+      stream.on('end', done);
+    });
+
+    //
+    // test: remove property
+    //
+    it('should remove nested property of JSON object', function(done) {
+      var stream = gulp.src('test/test.json').pipe(json(function(obj) {
+        delete obj.nested.name;
+        return obj;
+      }));
+      stream.on('error', done);
+      stream.on('data', function(file) {
+        var obj = JSON.parse(file.contents);
+        obj.nested.should.have.properties({
+          'version': '1.0.0'
+        });
+        obj.nested.should.not.have.property('name');
+      });
+      stream.on('end', done);
+    });
+
+  });
+});
